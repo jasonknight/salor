86e9cd4be4c1ab31616e062b90a39a05aa3db54b
Jason Knight <jason@jason-knight-martin.com>
2012-07-10 23:38:51 +0200

    Word wrap for license

--- beast.php
+++ beast.php
@@ -10,7 +10,12 @@
   default:
     $api_key = $_POST['api_key'];
     if (is_dir('./clients/' . $api_key)) {
-    
+      $payload = $_POST;
+      unset($payload['api_key']); 
+      foreach ($payload as $key=>$value) {
+        $drop = array( $key => $value);
+        $fp = fopen("./clients/$key",'w+');
+      }
     } else {
       header("Content-Type: application/javascript;");
       echo "_beast_lives = false;alert('You do not have an account with us.');";
